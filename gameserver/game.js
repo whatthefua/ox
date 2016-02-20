2e60b6bfbe6c926cca261fda4940173dab895026
Teerapath Sujitto <teerapart.s@gmail.com>
2016-02-21 01:51:07 +0700

    Add room system for game server

--- gameserver/game.js
+++ gameserver/game.js
@@ -1,7 +1,3 @@
-// var board = [];
-// var turn = 1;
-// var turnCount = 0;
-// var lastmove;
 var db = require('./db.js');
 function initBoard(){
   var board = [];
@@ -17,28 +13,97 @@ function initBoard(){
   var boardData = {};
   boardData.board = board;
   boardData.turn = 1;
+  boardData.turnCount = 0;
   return boardData;
+}
+function getDominator(arr){
+  var ans = [];
+  var s = arr.length;
+  var vertFlag = 0x1,horiFlag = 0x2,downFlag = 0x4,upFlag = 0x8;
+  var map = [0,1,16];
+  //Fuck this OX Logic
+  for(var i=0;i<s;i++){
+    ans[i] = [];
+    for(var j=0;j<s;j++){
+      ans[i][j]=0;
+      if(arr[i][j]==1){
+        if(i>0)ans[i][j] += ans[i-1][j]&vertFlag;
+        else ans[i][j] += map[arr[i][j]]<<0;
+        if(j>0)ans[i][j] += ans[i][j-1]&horiFlag;
+        else ans[i][j] += map[arr[i][j]]<<1;
+        if(i>0&&j>0)ans[i][j] += ans[i-1][j-1]&downFlag;
+        else if(i==0 && j ==0) ans[i][j] += map[arr[i][j]]<<2;
+        if(i>0&&j<s-1)ans[i][j] += ans[i-1][j+1]&upFlag;
+        else if(i==0 && j == s-1) ans[i][j] += map[arr[i][j]]<<3;
+      }
+      else if(arr[i][j]==2){
+        if(i>0)ans[i][j] += ans[i-1][j]&(vertFlag<<4);
+        else ans[i][j] += map[arr[i][j]]<<0;
+        if(j>0)ans[i][j] += ans[i][j-1]&(horiFlag<<4);
+        else ans[i][j] += map[arr[i][j]]<<1;
+        if(i>0&&j>0)ans[i][j] += ans[i-1][j-1]&(downFlag<<4);
+        else if(i==0 && j ==0) ans[i][j] += map[arr[i][j]]<<2;
+        if(i>0&&j<s-1)ans[i][j] += ans[i-1][j+1]&(upFlag<<4);
+        else if(i==0 && j == s-1) ans[i][j] += map[arr[i][j]]<<3;
+      }
+    }
+  }
+  for(var i=0;i<s;i++){
+    if(ans[i][s-1]||ans[s-1][i])return ans[i][s-1] <16 ? 1:2;
+  }
+}
+function getBlock(board,target){
+  return board[target.mi][target.mj][target.i][target.j];
+}
+function setBlock(board,target){
+  board[target.mi][target.mj][target.i][target.j] = parseInt(target.player);
+}
+function validateMove(boardState,move){
+  if(move.player!=='1'&&move.player!=='2') return "Who?!?";
+  if(boardState.ended) return "Game ended.";
+  if(getBlock(boardState.board,move)) return "Block occupied.";
+  if(getDominator(boardState.board[move.mi][move.mj])) return "This macro is fucking owned";
+  if(boardState.lastmove){
+
+  }
+}
+function setBoard(boardName,boardState,callback){
+  db.set(boardName+ ":state",JSON.stringify(boardState),(err,resp)=>{
+    if(err)return callback(err);
+    callback(null,boardState);
+  });
 }
 exports.getBoard = (boardName,callback)=>{
   db.get(boardName+":state",function(err,db_res){
     if(err)return console.log(err);
     if(!db_res){
       var newBoard = initBoard();
-      return db.set(boardName+":state",JSON.stringify(newBoard),function(err,resp){
+      setBoard(boardName,newBoard,(err,resp)=>{
         if(err)return callback(err);
         console.log("New board :" + boardName + " created");
         callback(null,JSON.stringify(newBoard));
       });
     }
     callback(null,db_res);
-  })
+  });
 }
-exports.move = (boardName,player,i,j,mi,mj,res)=>{
-  db.get(boardName+":state",function(err,db_res){
+exports.move = (move,callback)=>{
+  db.get(move.boardName+":state",(err,db_res)=>{
     if(err)return console.log(err);
-    if(!res)
-      return res.end("Board not exists! Unusual activities.");
-    var board = JSON.parse(db_res);
-    res.end(db_res);
+    if(!db_res)
+    return callback("Board not exists! Unusual activities.");
+    var boardState = JSON.parse(db_res);
+    var moveError = validateMove(boardState,move);
+    if(!moveError){
+      setBlock(boardState.board,move);
+      setBoard(move.boardName,boardState,(err,resp)=>{
+        if(err){
+          console.log("Database error:" + err);
+          return callback(new Error("Internal server error"));
+        }
+        callback(null,JSON.stringify(resp));
+      });
+    }
+    else callback(new Error(moveError));
   })
 }
--- gameserver/main.js
+++ gameserver/main.js
@@ -5,20 +5,6 @@ var bodyParser = require('body-parser');
 var db = require('./db.js');
 var game = require('./game.js');
 var app = express();
-var board = [];
-var turn = 1;
-var lastmove;
-function initBoard(){
-  for(var mi=0;mi<3;mi++){
-    board[mi]=[];
-    for(var mj=0;mj<3;mj++){
-      board[mi][mj]=[];
-      for(var i=0;i<3;i++){
-        board[mi][mj][i]=[0,0,0];
-      }
-    }
-  }
-}
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
@@ -28,46 +14,14 @@ app.get('/state',(req,res)=>{
   game.getBoard(queryData.boardName,function(err,data){
     if(err)return console.log(err);
     res.end(data);
-  })
-  // res.end(JSON.sboard)  ;
-  // res.end(JSON.stringify({
-  //   board:board,
-  //   turn:turn,
-  //   lastmove:lastmove
-  // }));
+  });
 });
-// app.post('/move',(req,res)=>{
-//   if(req.body.player!=turn){
-//     res.end("Not your turn.");
-//   }
-//   else{
-//     var move = req.body;
-//     // res.end(JSON.stringify(move));
-//     var owner = board[move.mi][move.mj][move.i][move.j];
-//     if(!lastmove||(lastmove.i==move.mi && lastmove.j==move.mj)){
-//       if(owner!=0)return res.end("Block owned by " + owner==1?"red":"blue");
-//       board[move.mi][move.mj][move.i][move.j] = move.player;
-//       if(!lastmove)lastmove={};
-//       lastmove.i = move.i;
-//       lastmove.j = move.j;
-//       lastmove.mi = move.mi;
-//       lastmove.mj = move.mj;
-//       turn = turn==1?2:1;
-//       res.end("Move ok");
-//     }
-//     else{
-//       res.end("Invalid move");
-//     }
-//   }
-//   // res.end(req.body.toString());
-//
-// });
 app.post('/move',(req,res)=>{
-  var move = req.body;
-  // game.move(req)
+  game.move(req.body,(err,data)=>{
+    if(err)return res.end(err.toString());
+    res.end(data);
+  });
 });
-// res.end(req.body.toString());
 
 app.use(express.static('./publics'));
-initBoard();
 app.listen(3000);
